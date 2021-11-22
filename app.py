e06dcb9e6394df66d8c9663aad0c2d06fd4dff71
Gregorio Zamora <86702253+jgregoza@users.noreply.github.com>
2021-11-22 12:01:15 -0600

    Add files via upload
    Mejora

--- app.py
+++ app.py
@@ -1,49 +1,68 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional, Text
 from datetime import date, datetime
 
+from starlette.status import HTTP_204_NO_CONTENT
 
-app = FastAPI()
+# Instanciado la app
+
+app = FastAPI(
+    title="REST API with FastAPI and MongoDB",
+    description="Simple REST API using fastapi and mongodb",
+    version="0.0.1"
+)
+
+# Arrego (lista) donde se almacenan los objetos o JSON
 
 postdb = []
 
-# post model
+# Post model (pydantic)
 
 class Post(BaseModel):
   id: int
   title: str
-  authon: str
+  author: str
   content: Text
   created_at: datetime = datetime.now()
   published_at: datetime
   published: Optional[bool] = False
 
+# Routes
 
-@app.get("/")
+@app.get("/", tags=["POST"])
 def read_root():
-  return {"home": "Home page"}
+    return {"home": "Home page"}
 
-@app.get("/blog")
+@app.get("/posts", response_model=list[Post], tags=["POST"])
 def get_posts():
-  return postdb
+    return postdb
 
-@app.post("/blog")
+@app.post("/posts", response_model=Post, tags=["POST"])
 def add_post(post: Post):
-  postdb.append(post.dict())
-  return postdb[-1]
+    postdb.append(post.dict())    # Se agrega al arreglo (postdb) como un dicccionario
+    return postdb[-1]   # Retornar el ultimo id recibido.
 
-@app.get("/blog/{post_id}")
+@app.get("/posts/{post_id}", response_model=Post, tags=["POST"])
 def get_post(post_id: int):
-    post = post_id - 1
-    return postdb[post]
+    for post in postdb:   # Se recorre el arreglo donde se almacenan
+        if post["id"] == post_id:   # Si el id del post es igual al que estas consultando
+            return post   # Retorna el post
+    #post = post_id - 1
+    #return postdb[post]
+    raise HTTPException(status_code=404, detail="Post Not Found")   # Error controlado si el ID no existe
 
-@app.post("/blog/{post_id}")
+@app.put("/posts/{post_id}", response_model=Post, tags=["POST"])
 def update_post(post_id: int, post: Post):
     postdb[post_id] = post
     return {"message": "Post has been updated succesfully"}
 
-@app.delete("/blog/{post_id}")
+@app.delete("/posts/{post_id}", status_code=HTTP_204_NO_CONTENT, tags=["POST"])
 def delete_post(post_id: int):
-    postdb.pop(post_id-1)
-    return {"message": "Post has been deleted succesfully"}
+    for index, post in enumerate(postdb):   # las listas empiezan en 0 por eso enumerate para eliminar por indice y no id
+      if post["id"] == post_id:
+        postdb.pop(index)
+        return {"message": "Post has been deleted succesfully"}
+    raise HTTPException(status_code=404, detail="Post Not Found")
+    #postdb.pop(post_id -1)
+    #return {"message": "Post has been deleted succesfully"}
